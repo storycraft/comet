2bd74774b59c02cabf7a539284567b061d3652f0
storycraft <storycraft@pancake.sh>
2025-11-08 10:59:05 +0900

    feat: add Layout

--- src/layout.rs
+++ src/layout.rs
@@ -1,6 +1,8 @@
 mod taffy;
 pub mod tree;
 
+use kurbo::{Point, Rect, Size};
+
 pub type ContainerLayoutFn = fn();
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
@@ -46,5 +48,50 @@ impl PartialEq for DisplayInner {
             (Self::Container(f1), Self::Container(f2)) => f1 == f2,
             _ => false,
         }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Layout {
+    /// Relative z-index of the layout box
+    pub z_index: u32,
+
+    /// Top-left location of the layout box
+    pub location: Point,
+
+    /// Bounding box size of the layout box
+    pub size: Size,
+
+    /// Rectangle representing the content area of the layout box.
+    /// It can be larger than `size` when content overflows.
+    pub content_size: Size,
+
+    /// The size of the borders of the layout box
+    pub border: Rect,
+
+    /// The size of the padding of the layout box
+    pub padding: Rect,
+
+    /// The size of the margin of the layout box
+    pub margin: Rect,
+}
+
+impl Layout {
+    pub fn new() -> Self {
+        Self {
+            z_index: 0,
+            location: Point::ORIGIN,
+            size: Size::ZERO,
+            content_size: Size::ZERO,
+            border: Rect::ZERO,
+            padding: Rect::ZERO,
+            margin: Rect::ZERO,
+        }
+    }
+}
+
+impl Default for Layout {
+    fn default() -> Self {
+        Self::new()
     }
 }
