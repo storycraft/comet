d013ba1d98d1f93fb083a3d100803eb6b1a36fdd
storycraft <storycraft@pancake.sh>
2026-02-23 12:20:25 +0900

    feat: return HitResult on hit_child, change hit_iter to not yield root node

--- crates/layout/src/tree/hit.rs
+++ crates/layout/src/tree/hit.rs
@@ -17,21 +17,20 @@ impl LayoutTree {
     }
 
     /// Perform hit testing relative to parent node.
-    pub fn hit_child(&self, parent: LayoutNodeKey, pos: (f64, f64)) -> Option<LayoutNodeKey> {
+    pub fn hit_child(&self, parent: LayoutNodeKey, pos: (f64, f64)) -> Option<HitResult> {
         for child_key in self.nodes.cursor(self.nodes.last_child(parent)).rev() {
-            let Some(child) = self.nodes.get(child_key) else {
-                continue;
-            };
-
-            if child.layout.hit(pos) {
-                return Some(child_key);
+            if let Some(inner_pos) = self.hit(child_key, pos) {
+                return Some(HitResult {
+                    node: child_key,
+                    inner_pos,
+                });
             }
         }
 
         None
     }
 
-    /// Starting from root node, iteratively search hit nodes.
+    /// From root node, iteratively search for hit child nodes.
     pub fn hit_iter(&self, root: LayoutNodeKey, pos: (f64, f64)) -> HitIter<'_> {
         HitIter {
             tree: self,
@@ -51,19 +50,18 @@ impl Iterator for HitIter<'_> {
     type Item = HitResult;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let node = self.next.take()?;
-        self.next_pos = self.tree.hit(node, self.next_pos)?;
-        self.next = self.tree.hit_child(node, self.next_pos);
+        let next = self.tree.hit_child(self.next.take()?, self.next_pos)?;
+        self.next = Some(next.node);
+        self.next_pos = next.inner_pos;
 
-        Some(HitResult {
-            node,
-            inner_pos: self.next_pos,
-        })
+        Some(next)
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct HitResult {
+    /// Target node
     pub node: LayoutNodeKey,
+    /// Hit position relative to node
     pub inner_pos: (f64, f64),
 }
